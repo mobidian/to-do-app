da202b2b5dd8e09eef1fe831b4458295de7739e3
orlando-c-h <orlando1409@gmail.com>
2017-11-08 13:18:15 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ToDoComponent } from './to-do/to-do.component';
+import { TodoService } from './to-do/to-do.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
